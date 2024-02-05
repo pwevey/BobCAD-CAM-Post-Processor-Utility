ab9546d8758ac755025d128a78ddd0a3c9c456fe
Renley <47496733+pwevey@users.noreply.github.com>
2024-02-05 13:15:27 -0500

    fixed a bug where it would not show a hover box if no jobType was defined for the post block

--- src/extension.js
+++ src/extension.js
@@ -12,32 +12,29 @@ function getJsonFilePath() {
   return path.join(extensionRoot, 'res', 'post_data', 'postBlocks.json');
 }
 
-
-
-
 function activate(context) {
-  //console.log('Extension activated');
-
   // Register hover provider for .bcpst documents
   const hoverProvider = vscode.languages.registerHoverProvider('bcpst', {
-    provideHover(document, position, token) {
-      //console.log('Hover requested');
-
+    provideHover(document, position) {
       // Check if the line starts with a post block number
       const postBlocks = readPostBlocks();
       if (postBlocks) {
         const line = document.lineAt(position.line).text;
-        //console.log('Hover Line:', line);
 
-        const postBlockNumber = parseInt(line);
-        //console.log('Post Block Number:', postBlockNumber);
+        const postBlockNumberMatch = line.match(/^(\d+)/);
+        if (postBlockNumberMatch) {
+          const postBlockNumber = parseInt(postBlockNumberMatch[1]);
+          // console.log('Post Block Number:', postBlockNumber);
 
-        const hoveredBlock = postBlocks.blocks.find((block) => block.number === postBlockNumber);
-        //console.log('Hovered Block:', hoveredBlock);
+          const hoveredBlock = postBlocks.blocks.find((block) => block.number === postBlockNumber);
 
-        if (hoveredBlock) {
-          const hoverText = new vscode.MarkdownString(`**${hoveredBlock.name}**\n\n${hoveredBlock.description}\n\nJob Types: ${hoveredBlock.jobTypes.join(', ')}`);
-          return new vscode.Hover(hoverText);
+          if (hoveredBlock) {
+            let hoverText = new vscode.MarkdownString(`**${hoveredBlock.name}**\n\n${hoveredBlock.description}`);
+            if (hoveredBlock.jobTypes) {
+              hoverText.appendMarkdown(`\n\nJob Types: ${hoveredBlock.jobTypes.join(', ')}`);
+            }
+            return new vscode.Hover(hoverText);
+          }
         }
       }
 
@@ -53,15 +50,10 @@ function activate(context) {
 function readPostBlocks() {
   const jsonFilePath = getJsonFilePath();
 
-  // "C:/Users/pweve/Documents/GitHub/BobCAD-CAM-Post-Processor-Utility/res/post_data/postBlocks.json"
-
   try {
-    //console.log('Reading JSON file from path:', jsonFilePath);
     const jsonContent = fs.readFileSync(jsonFilePath, 'utf-8');
-    //console.log('Parsed JSON:', jsonContent);
     return JSON.parse(jsonContent);
   } catch (error) {
-    //console.error('Error reading/parsing JSON file:', error.message);
     return null;
   }
 }
