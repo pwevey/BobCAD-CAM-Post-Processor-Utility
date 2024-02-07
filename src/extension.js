83be1566c0daf6f3f2843f22b1f28f3ffa588908
Renley <47496733+pwevey@users.noreply.github.com>
2024-02-07 16:14:31 -0500

    Fixed issue with go to post block in  command pallete. It now works

--- src/extension.js
+++ src/extension.js
@@ -1,27 +1,23 @@
 const vscode = require('vscode');
 const { PostBlockDataProvider } = require('./postBlocksTreeView');
-const HoverProvider = require('./hover'); // Import the HoverProvider
+const HoverProvider = require('./hover');
 
 let postBlockDataProvider;
 let postBlockTreeView;
 
 function activate(context) {
-  // Register hover provider for .bcpst documents
   const hoverProvider = vscode.languages.registerHoverProvider(['bcpst', 'source.bcpst'], new HoverProvider());
   context.subscriptions.push(hoverProvider);
 
   postBlockDataProvider = new PostBlockDataProvider();
 
-  // Register the "Refresh Post Blocks" command
   const refreshCommand = vscode.commands.registerCommand('postBlocks.refresh', () => {
     postBlockDataProvider.manualRefresh();
   });
   context.subscriptions.push(refreshCommand);
 
-  // Register the command handler for navigating to a line
   const navigateToLineCommand = vscode.commands.registerCommand('postBlocks.navigateToLine', (postBlockNumber) => {
     const lineNumber = postBlockDataProvider.findLineNumber(postBlockNumber);
-
     if (lineNumber !== undefined && vscode.window.activeTextEditor) {
       const position = new vscode.Position(lineNumber, 0);
       const selection = new vscode.Selection(position, position);
@@ -31,17 +27,28 @@ function activate(context) {
   });
   context.subscriptions.push(navigateToLineCommand);
 
-  // Register the Post Blocks tree view
+  const navigateToLineFromPaletteCommand = vscode.commands.registerCommand('postBlocks.navigateToLineFromPalette', async () => {
+    const postBlockNumber = await vscode.window.showInputBox({ prompt: 'Enter Post Block Number' });
+    if (postBlockNumber !== undefined && !isNaN(postBlockNumber)) {
+      const lineNumber = postBlockDataProvider.findLineNumber(parseInt(postBlockNumber));
+      if (lineNumber !== undefined && vscode.window.activeTextEditor) {
+        const position = new vscode.Position(lineNumber, 0);
+        const selection = new vscode.Selection(position, position);
+        vscode.window.activeTextEditor.selection = selection;
+        vscode.window.activeTextEditor.revealRange(selection, vscode.TextEditorRevealType.InCenter);
+      }
+    }
+  });
+  context.subscriptions.push(navigateToLineFromPaletteCommand);
+
   postBlockTreeView = vscode.window.createTreeView('postBlocks', { treeDataProvider: postBlockDataProvider });
   context.subscriptions.push(postBlockTreeView);
 
-  // Register the "Collapse All Folders" command
   const collapseAllCommand = vscode.commands.registerCommand('postBlocks.collapseAll', () => {
     postBlockDataProvider.collapseAll();
   });
   context.subscriptions.push(collapseAllCommand);
 
-  // Register the "Expand All Folders" command
   const expandAllCommand = vscode.commands.registerCommand('postBlocks.expandAll', () => {
     postBlockDataProvider.expandAll();
   });
